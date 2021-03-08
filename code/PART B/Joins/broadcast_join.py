ed23207f5c38b4fd97125824932e2af0626db396
FayStatha <fay.statha@gmail.com>
2021-03-08 06:47:09 +0200

    broadcast join

---
+++ code/PART B/Joins/broadcast_join.py
@@ -0,0 +1,45 @@
+from pyspark.sql import SparkSession
+from io import StringIO
+import csv
+import sys
+
+spark = SparkSession.builder.appName("broadcast-join").getOrCreate()
+
+sc = spark.sparkContext
+
+def join(x):
+    result = []
+    for v in table_broad.value:
+        if v[key1_index] == x[0]:
+            new = (x[0], tuple(x[1]), tuple(v[:key1_index] + v[key1_index+1:]))
+            result.append(new)
+    return result
+
+# The user must give the file path of each file as input with the index of the key for each one
+
+# For example:
+# spark-submit repartition_join.py "hdfs://master:9000/movies/movie-genres-100.csv" 0 "hdfs://master:9000/movies/ratings.csv" 1
+# to join movie-genres-100 with ratings on movie id
+
+file1 = sys.argv[1]
+
+key1_index = int(sys.argv[2])
+
+file2 = sys.argv[3]
+
+key2_index = int(sys.argv[4])
+
+table1 = \
+        sc.textFile(file1). \
+        map(lambda x : x.split(',')).collect()
+
+table_broad = sc.broadcast(table1)
+
+table2 = \
+        sc.textFile(file2). \
+        map(lambda x : (x.split(','))). \
+        map(lambda x : (x.pop(key2_index), x)). \
+        flatMap(lambda x : join(x)).collect()
+
+for i in table2:
+        print(i)
